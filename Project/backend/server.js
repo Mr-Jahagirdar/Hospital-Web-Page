4887a85436feca9cb68a84f01a767d1f84f979d2
faizan khodil <faizankhodil01@gmail.com>
2026-05-18 18:32:27 +0530

    Update server.js

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -15,7 +15,7 @@ app.use(rateLimiter);
 
 app.use('/api/chatbot', chatbotRoutes);
 
-const PORT = https://hospital-web-page1.onrender.com/;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
